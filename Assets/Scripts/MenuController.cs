8fa3985edb8c761b5ec30398582338ae8fb813b3
Danil Bykon <bykon.danil@gmail.com>
2020-07-08 15:56:41 +0300

    Adding scroll

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 public class MenuController : MonoBehaviour
 {
     public GameObject[] menuList;
-
-    private GameObject _menuLast;
-
-    private GameObject _menuCurrent;
+   
+    public GameObject _menuLast;
+   
+    public GameObject _menuCurrent;
 
     public void toSaveMenu()
     {
@@ -46,6 +46,49 @@ public class MenuController : MonoBehaviour
             _menuCurrent = menuList[3];
         }
     
+    }
+
+    public void comeBack()
+    {
+           GameObject tmp;
+          _menuCurrent.SetActive(false);
+          _menuLast.SetActive(true);
+           tmp = _menuCurrent;
+          _menuCurrent = _menuLast;
+          if (tmp == menuList[3])
+          {
+               _menuLast = menuList[0];
+          }
+          else
+          {
+               _menuLast = tmp;
+          }
+        
+        /* if (_menuLast == menuList[1] && _menuCurrent == menuList[3])
+         {   
+             menuList[3].SetActive(false);
+             menuList[1].SetActive(true);
+             _menuLast = menuList[3];
+             _menuCurrent = menuList[1];
+         }
+         if (_menuLast == menuList[2] && _menuCurrent == menuList[3])
+         {
+             menuList[3].SetActive(false);
+             menuList[2].SetActive(true);
+             _menuLast = menuList[3];
+             _menuCurrent = menuList[2];
+         }
+         if (_menuCurrent == menuList[1] && _menuLast == menuList[0] )
+         {
+             menuList[1].SetActive(false);
+             menuList[0].SetActive(true);
+         }
+         if (_menuCurrent == menuList[2] && _menuLast == menuList[0])
+         {
+             menuList[2].SetActive(false);
+             menuList[0].SetActive(true);
+         }*/
+
     }
 
 }
---
+++ Assets/Scripts/ScrollViewAdapterExample.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollViewAdapterExample : MonoBehaviour
+{
+    public RectTransform prefab;
+    public Text countText;
+    public ScrollRect scrolView;
+    public RectTransform content;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateItems() {
+        int newCount;
+        int.TryParse(countText.text, out newCount);
+        StartCoroutine(FetchItemModelsFromServer(newCount, results => OnReceivedNewModels(results)));
+    }
+
+    void OnReceivedNewModels(ExampleItemModel[] models)
+    {
+        foreach (var child in content.transform)
+        {
+            Destroy(child);
+        }
+    }
+
+    IEnumerator FetchItemModelsFromServer(int count, Action<ExampleItemModel[]> onDone) {
+
+        yield return new WaitForSeconds(2f);
+
+        var result = new ExampleItemModel[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            result[i] = new ExampleItemModel();
+            result[i].title = "Item " + i;
+        }
+    }
+
+    public class ExampleItemModel {
+        public string title;
+    }
+}
